b51d232318ff4346e6625e1094234069809845bb
jieun822 <solvit0822@naver.com>
2023-07-11 17:01:02 +0900

    경로 수정

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
+// aos 플러그인 초기화
 AOS.init();
 
+// 부드럽게 올라가기
 $(".btn").on("click", (e) => {
   e.preventDefault();
   scrollTo({
@@ -8,6 +10,7 @@ $(".btn").on("click", (e) => {
   });
 });
 
+// 첫 화면 지났을 때 위로 가기 버튼 나오도록
 $(window).on("scroll", () => {
   let scrollHt = $(document).scrollTop();
 
