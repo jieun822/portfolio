a4146af919c69f934361460b324a5756e5db1716
jieun822 <solvit0822@naver.com>
2023-07-12 16:35:01 +0900

    230712 수정

--- project1/project1.js
+++ project1/project1.js
@@ -26,3 +26,11 @@ var swiper = new Swiper(".mySwiper", {
     1200: { slidesPerView: 5 },
   },
 });
+
+/* // 햄버거메뉴 열고 닫기
+$(".checkbox").on("click", () => {
+  $(".header").toggleClass("active");
+  $(".logo").toggleClass("active");
+  $(".checkmark span").toggleClass("active");
+  $(".menu").slideToggle(); // == display: block
+}); */
