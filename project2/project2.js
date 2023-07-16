1363bb6cf4ff4fc6c2b9241c3d8d9ae931826f7b
jieun822 <solvit0822@naver.com>
2023-07-16 17:21:49 +0900

    서브페이지2 수정

--- project2/project2.js
+++ project2/project2.js
@@ -9,3 +9,14 @@ $(".btn").on("click", (e) => {
     behavior: "smooth",
   });
 });
+
+// 첫 화면 지났을 때 위로 가기 버튼 나오도록
+$(window).on("scroll", () => {
+  let scrollHt = $(document).scrollTop();
+
+  if (scrollHt > 240) {
+    $(".btn").css({ opacity: 1, pointerEvents: "all" });
+  } else {
+    $(".btn").css({ opacity: 0, pointerEvents: "none" });
+  }
+});
